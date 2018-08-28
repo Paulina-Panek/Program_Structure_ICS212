446574bd01574b554aa19fdd216bd4848fb25b10
Paulina Panek <39210600+Paulina-Panek@users.noreply.github.com>
2018-08-27 16:58:05 -1000

    Add files via upload

---
+++ homework0.c
@@ -0,0 +1,29 @@
+/*****************************************************************
+//  //  NAME:        Paulina Panek
+//  //
+//  //  HOMEWORK:    0
+//  //
+//  //  CLASS:       ICS 212
+//  //
+//  //  INSTRUCTOR:  Ravi Narayan
+//  //
+//  //  DATE:        August 28, 2018
+//  //
+//  //  FILE:        homework0.c
+//  //
+//  //  DESCRIPTION:
+//  //   Simple hello world program in C
+//  //
+//  //
+//  *************************************************************/
+
+#include <stdio.h>
+
+int main()
+{
+    printf("\\Name:\tPaulina Panek\\\n");
+    printf("\\Hello World\\\n");
+    printf("\\ICS212 Program Structure\\\n");
+
+    return(0);
+}
