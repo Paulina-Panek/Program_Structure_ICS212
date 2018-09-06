9c40433146e91f2a5daa9b87870bdaaa82902790
Paulina Panek <39210600+Paulina-Panek@users.noreply.github.com>
2018-09-05 17:09:33 -1000

    Add files via upload

--- homework1/exercise1.c
+++ homework1/exercise1.c
@@ -12,11 +12,12 @@
 //  //  FILE:        homework1.c
 //  //
 //  //  DESCRIPTION:
-//  //   A simple program listing factors of an integer given
+//  trcmp/   A simple program listing factors of an integer given
 //  //   by user interface
 //  //
 //  //
 //  *************************************************************/
+
 
 #include <stdio.h>
 #include <stdlib.h> /*atoi*/
@@ -24,68 +25,83 @@
 int isDivisible(int, int);
 
 /***************************************************************
-//  Function name: isDivisible(user_int, i)
+//  Function name: isDivisible(number, PotentialFactor)
 //  //
-//  //  DESCRIPTION:   Checks if the user integer (user_int) is divisible by
-//  //                 integers from 1 to user_int
+//  //  DESCRIPTION:   Checks if <number> is divisible by
+//  //                 <PotentialFactor>
 //  //
-//  //  Parameters:    user_int : integer entered by the user
-//  //                               
+//  //  Parameters:    number : looking for factors of
+//  //                 PotentialFactor : checked if is a factor              
 //  //
-//  //  Return values: a table with indexed factors of user_int
-//  //                 
+//  //  Return values: 0 : <PotentialFactor> is NOT a factor
+//  //                 1 : <PotentialFactor> is a factor
 //  //
 ****************************************************************/
 
 int main()
-{
-    
-    int user_int, i, index;
+/***************************************************************
+//  Function name: main()
+//  //
+//  //  DESCRIPTION:   Userinteface function, obtains an integer
+//  //                 from user and generates an indexed table
+//  //                 of factors
+//  //
+//  //  Parameters:    input[12] : input from the command line
+//  //                 user_int : integer entered by the user
+//  //                 obtained from input[]
+//  //                             
+//  //
+//  //  Return values: always 0
+//  //                 
+//  //
+****************************************************************/
+{  
+    int user_int, i, index, factor;
     char input[12];
 
     printf("This program calulates the factors of an integer provided by the user.\n");
     printf("Please enter an integer greater than 0:\n");
-    
 
     fgets(input, 12, stdin);
-     user_int = atoi(input);
+    user_int = atoi(input);
 
-    while (user_int == 0 || user_int <= 0)
+    while (user_int <= 0)
+    /*Ensures input not a character/string/0/negative integer*/
     {  
-        if  (user_int == 0)
-            {
-            printf("You entered character(s). Please enter an integer.\n");
-            fgets(input, 12, stdin);
-            user_int = atoi(input);
-            }  
-
-        else if (user_int <= 0)
-        {  
-            printf("Please enter a POSITIVE integer to check its factors:\n");
-            fgets(input, 12, stdin);
-            user_int = atoi(input);         
-        }
+        printf("Incorrect input. Please enter a POSITIVE integer.\n");
+        fgets(input, 12, stdin);
+        user_int = atoi(input);
     }
        
     printf("You entered: %d\n\n", user_int);
 
+    index = 1;
+
     printf("%10s", "Index");
     printf("%10s\n", "Factor");
 
-    index = 1;
-
     for (i = 1; i <= user_int; i++)
     {             
-                    
-        int isDivisible(int user_int, int i);
+        factor = isDivisible(user_int, i);
+                
+        if (factor ==1)
         {
-            if (user_int%i == 0)
-                {
-                    printf("%10d%10d\n", index, i);
-                    index++;
-                }                 
-        } 
+            printf("%10d%10d\n", index, i);
+            index++;
+        }
     }
 
     return(0);
 }
+
+int isDivisible(int number, int PotentialFactor)
+{ 
+    int result;
+ 
+    if (number % PotentialFactor == 0)
+        result = 1;
+    else
+        result = 0;                 
+
+    return(result);
+} 
