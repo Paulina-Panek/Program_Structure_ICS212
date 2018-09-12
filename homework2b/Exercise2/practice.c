56eb84c3da3bf1165e4bf429e3299216e0294a58
Paulina Panek <39210600+Paulina-Panek@users.noreply.github.com>
2018-09-12 13:20:23 -1000

    Add files via upload

--- homework2b/Exercise2/practice.c
+++ homework2b/Exercise2/practice.c
@@ -1,1 +1,43 @@
+#include <stdio.h>
+#include <string.h>
 
+
+int main ()
+{
+    char first[20], last[20],  username[6], ifshort[6];
+    int length, howShort;
+
+    printf("Please enter your first name:\n");
+    scanf("%s", first);
+
+    printf("Please enter your last name:\n");
+    scanf("%s", last);
+
+/* Generating username consisting of 6 characters */
+
+    /* 1. STRCAT: Combine last with first */
+
+    strcat(last, first);
+    
+    /* 2. STRLEN: Check lenght of appended string */
+
+    length = strlen(last); 
+
+    /* 3. STRCPY 4. STRNCAT If shorter than 6 characters, add 0s at the end.
+       5. STRNCPY If longer than 6, take only the first 6 */
+
+    if (length < 6)
+    {
+        strcpy(ifshort, "000000");
+        howShort = 6 - length;
+        strncat(last, ifshort, howShort);
+    }
+
+        strncpy(username, last, 6);
+
+
+    printf("Your username is:\t%s\n", username);
+
+ 
+return(0);
+}
