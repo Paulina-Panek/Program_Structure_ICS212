4bc174bfeaed0f5928bb7cc79d0043490709f65c
Paulina Panek <39210600+Paulina-Panek@users.noreply.github.com>
2018-09-12 23:28:27 -1000

    Add files via upload

--- homework2b/Exercise1/driver.c
+++ homework2b/Exercise1/driver.c
@@ -1,13 +1,45 @@
+/***************************************************************
+//  //  NAME:        Paulina Panek
+//  //
+//  //  HOMEWORK:    2b
+//  //
+//  //  CLASS:       ICS 212
+//  //
+//  //  INSTRUCTOR:  Ravi Narayan
+//  //
+//  //  DATE:        September 13, 2018
+//  //
+//  //  FILE:        driver.c
+//  //
+//  //  DESCRIPTION:
+//  //   This file contains the driver for halfstring function. Uses
+//  //   user-input to test the functon.
+//  //
+//  ****************************************************************/
+
 #include "halfstring.h"
 #include <stdio.h>
 
+/***************************************************************
+//  Function name: main()
+//  //
+//  //  DESCRIPTION:   Driver to test function halfstring 
+//  //                 
+//  //
+//  //  Parameters:    none
+//  //                             
+//  //
+//  //  Return values: always 0
+//  //                
+****************************************************************/
+
 int main()
 {
-    int i, size_new;
+    int  i, j, size_new;
     char string_old[200], string_new[101];
 
     printf("Enter the string you want to process:\n");
-    fgets( string_old, 200, stdin);
+    fgets(string_old, 200, stdin);
 
     i = 0;
 
@@ -18,15 +50,16 @@ int main()
 
     size_new = (i/2);
 
-
     string_new[size_new] ='\0';
     
     for (i = 0; i < size_new; i++)
     {
-        string_new[i] = 1;
+        string_new[i] = 'a';
+
+    /* string_new here is just an array place holder */
 
     } 
-
+    
     halfstring(string_old, string_new);
 
     printf("Old string:\t%s\n", string_old);
--- homework2b/Exercise1/halfstring.c
+++ homework2b/Exercise1/halfstring.c
@@ -1,4 +1,38 @@
+/*****************************************************************
+//  //  NAME:        Paulina Panek
+//  //
+//  //  HOMEWORK:    2b
+//  //
+//  //  CLASS:       ICS 212
+//  //
+//  //  INSTRUCTOR:  Ravi Narayan
+//  //
+//  //  DATE:        September 13th, 2018
+//  //
+//  //  FILE:        halfstring.c
+//  //
+//  //  DESCRIPTION:
+//  //  Function that takes every other character from string_old   
+//  //  and creates string_new from it.
+//  //
+//  *************************************************************/
+
 #include <stdio.h>
+
+/***************************************************************
+//  Function name: halfstring()
+//  //
+//  //  DESCRIPTION:   Takes every other character of initial string 
+//  //                 and creates a new string from them. Only works
+//  //                 when a string properly terminated with a '\0'.
+//  //
+//  //  Parameters:    string_old(char []) initial string
+//  //                 string_new(char []) string created from
+//  //                 every other character of string_new
+//  //
+//  //  Return values: none (void)
+//  //                
+****************************************************************/
 
 void halfstring(const char string_old[], char string_new[])
 {
