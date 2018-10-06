7c2f64cf6e1e4be76a1f752b6b4357a6c9616af3
Paulina Panek <39210600+Paulina-Panek@users.noreply.github.com>
2018-10-05 20:03:00 -1000

    Delete header.h

--- homework3b/header.h
+++
@@ -1,9 +0,0 @@
-/*int addRecord (struct record**, int, char [], char []);
-
-int printRecord (struct record*, int);
-
-void printAllRecords (struct record*);
-
-int deleteRecord (struct record**, int ); */
-
-void getaddress (char[], int);
