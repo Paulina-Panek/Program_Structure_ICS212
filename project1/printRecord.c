5cd1ce685affdee85145cf3d89f6f0417b30e552
Paulina Panek <39210600+Paulina-Panek@users.noreply.github.com>
2018-10-21 15:41:54 -1000

    Add files via upload

--- project1/printRecord.c
+++ project1/printRecord.c
@@ -30,6 +30,50 @@ extern int debugmode;
 
 int printRecord (struct record * start, int uaccountno)
 {
-return(0);
+    struct record * temp_old, *temp_new;
+    int rtrn_val, value;
+
+rtrn_val = -1;
+
+    if (start != NULL)
+    {
+        temp_old = start;
+        value = start->accountno;
+
+        while (value < uaccountno)
+        { 
+            temp_new = temp_old->next;
+
+            if (temp_new == NULL)
+            {
+                value = uaccountno + 1;
+            }
+            else
+            {
+                value = temp_new->accountno;
+
+                if (value < uaccountno)
+                {
+                    temp_old = temp_new;
+                }
+            }
+             
+        }
+        while (value == uaccountno)
+        {
+            rtrn_val = 0;
+            
+            printf("Account No:\t%d", uaccountno);
+            printf("Name:\t%s", temp_new->name);
+            printf("Address:\t%s", temp_new->address);
+                if (temp_new != NULL)
+                    value = temp_new->accountno ;
+                else
+                    value = uaccountno + 1;
+        }
+
+    }
+return(rtrn_val);
+
 }
 
